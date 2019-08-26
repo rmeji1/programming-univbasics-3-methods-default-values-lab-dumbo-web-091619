f17fd725edbf2d03b59440dd30b195e7278d623e
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-26 23:14:56 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,5 @@ def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
   retValue = "A plate of #{protein} with #{veg1} and #{veg2}." 
   puts retValue
-  sentence
+  return sentence
 end
